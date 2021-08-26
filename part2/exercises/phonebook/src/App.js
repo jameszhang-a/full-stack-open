cf96efd4db4d8e303d785ff497803663bc7992bc
James Zhang <jameszhanga@gmail.com>
2021-08-25 19:49:35 -0700

    Finish 2.9, all features includin filter complete

--- part2/exercises/phonebook/src/App.js
+++ part2/exercises/phonebook/src/App.js
@@ -1,14 +1,22 @@
 import React, { useState } from 'react';
 
 const App = () => {
-  const [ person, setPerson ] = useState([ { name: 'Arto Hellas' } ]);
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ]);
   const [ newName, setNewName ] = useState('');
+  const [ newNum, setNewNum ] = useState('');
+  const [ search, setSearch ] = useState('');
 
   const handleSubmit = (e) => {
     // Stops refresh on submit
     e.preventDefault();
     const newPerson = {
-      name : newName
+      name   : newName,
+      number : newNum
     };
 
     // Check for duplicates
@@ -20,26 +28,49 @@ const App = () => {
     console.log(containsPerson());
 
     // Adds new person to state
-    setPerson(person.concat(newPerson));
+    setPersons(persons.concat(newPerson));
     // Clears field
     setNewName('');
   };
 
-  const handleFormChange = (e) => {
+  const handleNameChange = (e) => {
     setNewName(e.target.value);
   };
 
+  const handleNumChange = (e) => {
+    setNewNum(e.target.value);
+  };
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const foundPerson = persons.filter((person) =>
+    person.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   const containsPerson = () => {
     // `some` returns true if any returns true
-    return person.some((curr) => curr.name.localeCompare(newName) === 0);
+    return persons.some((curr) => curr.name.localeCompare(newName) === 0);
   };
 
   return (
     <div className='App'>
       <h2>Phonebook</h2>
+
+      <h3>Search by name</h3>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input value={search} onChange={handleSearch} />
+      </form>
+
+      <h2>Add Entry</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          name: <input value={newName} onChange={handleFormChange} />
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+
+        <div>
+          number: <input value={newNum} onChange={handleNumChange} />
         </div>
 
         <div>
@@ -49,8 +80,12 @@ const App = () => {
 
       <h2>Numbers</h2>
       <div>
-        {person.map((person) => {
-          return <div key={person.name}>{person.name}</div>;
+        {foundPerson.map((person) => {
+          return (
+            <div key={person.name}>
+              {person.name} {person.number}
+            </div>
+          );
         })}
       </div>
     </div>
