4edcc56cc8f747bc603a2bdb3733dd9002127050
James Zhang <jameszhanga@gmail.com>
2021-08-25 19:20:53 -0700

    Finish 2.8, using 'some' to check for dups

--- part2/exercises/phonebook/src/App.js
+++ part2/exercises/phonebook/src/App.js
@@ -1,5 +1,60 @@
-function App() {
-  return <div className='App'>hello</div>;
-}
+import React, { useState } from 'react';
+
+const App = () => {
+  const [ person, setPerson ] = useState([ { name: 'Arto Hellas' } ]);
+  const [ newName, setNewName ] = useState('');
+
+  const handleSubmit = (e) => {
+    // Stops refresh on submit
+    e.preventDefault();
+    const newPerson = {
+      name : newName
+    };
+
+    // Check for duplicates
+    if (containsPerson()) {
+      alert(`${newName} is already in the phonebook`);
+      setNewName('');
+      return;
+    }
+    console.log(containsPerson());
+
+    // Adds new person to state
+    setPerson(person.concat(newPerson));
+    // Clears field
+    setNewName('');
+  };
+
+  const handleFormChange = (e) => {
+    setNewName(e.target.value);
+  };
+
+  const containsPerson = () => {
+    // `some` returns true if any returns true
+    return person.some((curr) => curr.name.localeCompare(newName) === 0);
+  };
+
+  return (
+    <div className='App'>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name: <input value={newName} onChange={handleFormChange} />
+        </div>
+
+        <div>
+          <button type='submit'>add</button>
+        </div>
+      </form>
+
+      <h2>Numbers</h2>
+      <div>
+        {person.map((person) => {
+          return <div key={person.name}>{person.name}</div>;
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default App;
